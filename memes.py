f2699fd6911253b84fa299461a07e0b79de956fe
Will Russell <will@will-russell.com>
2020-04-07 17:08:53 +0100

    Memes are finalised

--- memes.py
+++ memes.py
@@ -1,4 +1,7 @@
 import random
 
-def get_meme():
+def get_random():
     return random.randint(1,5)
+
+def get_meme():
+    return "21, got em"
--- workshop.py
+++ workshop.py
@@ -1,5 +1,8 @@
-print("Hello Hack Quarantine!")
-
+import memes
 
 def main():
-  print("This is the next step of the workshop!")
+    print("This is the next step of the workshop!")
+    print(memes.get_random())
+    print(memes.get_meme())
+
+main()
